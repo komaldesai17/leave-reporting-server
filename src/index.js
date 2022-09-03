511c4ed461783c0996107fff82da041c6c2bda25
KomalDesai <kdesai9895@gmail.com>
2022-09-03 21:40:52 +0530

    port

--- src/index.js
+++ src/index.js
@@ -55,7 +55,7 @@ app.use('/api', apiNotFound);
 app.use(pageNotFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app
     .listen(PORT, () => {
